0c66ec579756ba3a703542d152bedf7b0ee4be98
Tommy Schnabel <tommyschnabel@gmail.com>
2023-08-20 22:25:12 -0400

    more sudo

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir /etc/rpi-utc-clock
-cp ./clock.py /etc/rpi-utc-clock
-cp ./Roboto-Medium.ttf /etc/rpi-utc-clock
+sudo mkdir /etc/rpi-utc-clock
+sudo cp ./clock.py /etc/rpi-utc-clock
+sudo cp ./Roboto-Medium.ttf /etc/rpi-utc-clock
 
-cp ./utc-clock.service /etc/systemd/system/
+sudo cp ./utc-clock.service /etc/systemd/system/
 sudo systemctl enable utc-clock.service
 sudo systemctl start utc-clock.service
